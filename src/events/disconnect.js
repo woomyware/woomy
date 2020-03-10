b9b14a45d2662911b449e22200e4fc1c2dd04e07
mudkipscience <me@emily.im>
2020-03-10 12:01:44 +1100

    Delete disconnect.js, reconnection.js

--- src/events/disconnect.js
+++
@@ -1,3 +0,0 @@
-module.exports = client => { // logs if the bot gets disconnected from discord.
-	client.logger.warn(`Bot disconnected at ${new Date()}`);
-};
--- src/events/reconnection.js
+++
@@ -1,3 +0,0 @@
-module.exports = client => { // logs when the bot reconnects to discord
-	client.logger.info(`Reconnecting at ${new Date()}`);
-};
