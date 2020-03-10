f032e0c1e9d1be82a263460603a6a6c4959f0c80
Terryiscool160 <48654513+Terryiscool160@users.noreply.github.com>
2020-03-10 00:18:05 +0000

    More stuff.
    Added a dice roll command.

---
+++ src/commands/diceroll.js
@@ -0,0 +1,27 @@
+exports.run = async (bot, message, args) => {
+    if (args.length === 0) {
+      message.channel.send(`🎲 The dice landed on ${Array.from(Array(6).keys()).random() + 1}.`);
+    } else {
+      if (args[0].match(/^\d+$/)) {
+        message.channel.send(`🎲 The dice landed on ${Array.from(Array(parseInt(args[0])).keys()).random() + 1}.`);
+      } else {
+        message.channel.send(`🎲 The dice landed on ${Array.from(Array(6).keys()).random() + 1}.`);
+      }
+    }
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["diceroll"],
+    permLevel: "User",
+    requiredPerms: []
+  };
+  
+  exports.help = {
+    name: "dice",
+    category: "Fun",
+    description: "Rolls a dice.",
+    usage: "dice"
+  };
+  
