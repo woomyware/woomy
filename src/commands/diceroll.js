76d6230e97777e6b00dc33ba2e8ea6da957bc1a0
mudkipscience <me@emily.im>
2020-03-10 00:42:20 +0000

    Update diceroll.js

--- src/commands/diceroll.js
+++ src/commands/diceroll.js
@@ -1,11 +1,11 @@
 exports.run = async (bot, message, args) => {
     if (args.length === 0) {
-      message.channel.send(`🎲 The dice landed on ${Array.from(Array(6).keys()).random() + 1}.`);
+      message.channel.send(`🎲 You rolled a ${Array.from(Array(6).keys()).random() + 1}!`);
     } else {
       if (args[0].match(/^\d+$/)) {
-        message.channel.send(`🎲 The dice landed on ${Array.from(Array(parseInt(args[0])).keys()).random() + 1}.`);
+        message.channel.send(`🎲 You rolled a ${Array.from(Array(parseInt(args[0])).keys()).random() + 1}!`);
       } else {
-        message.channel.send(`🎲 The dice landed on ${Array.from(Array(6).keys()).random() + 1}.`);
+        message.channel.send(`🎲 You rolled a ${Array.from(Array(6).keys()).random() + 1}!`);
       }
     }
   };
