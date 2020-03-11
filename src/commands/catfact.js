cab212cecc136c206097020a93fe88e870a6e1b4
Terryiscool160 <48654513+Terryiscool160@users.noreply.github.com>
2020-03-11 20:52:43 +0000

    Add files via upload

--- src/commands/catfact.js
+++ src/commands/catfact.js
@@ -1,10 +1,16 @@
 const request = require("request");
 
 exports.run = async (bot, message, args) => {
+  message.channel.startTyping();
+  try{
   request({ uri: "https://catfact.ninja/facts", json: true }, (error, response, body) => {
-    if (error) throw new Error(error);
     message.channel.send(`**Did you know?**\n ${body.data[0].fact}`);
+    message.channel.startTyping();
   });
+} catch(err) {
+  message.channel.send(`<:error:466995152976871434> API error: ${err}`);
+  message.channel.stopTyping();
+};
 };
 
 exports.conf = {
