29e8198126ed6c82ef6881604c7015b582e13296
FLGX <flgx16@gmail.com>
2020-03-21 10:46:51 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/commands/ship.js
+++ src/commands/ship.js
@@ -9,43 +9,59 @@ exports.run = async (client, message, args) => {
   }
   message.channel.startTyping();
 
-  var user = client.getUserFromMention(args[0])
-  var user2 = client.getUserFromMention(args[1])
+  let users = [];
+  let totalLength = 0;
 
-  if (!user) {
-    let users;
-    users = client.searchForMembers(message.guild, args[0]);
-    if (users.length > 1)
-      return message.channel.send(
-        "<:error:466995152976871434> Found multiple users! Please be more specific or mention the user instead."
-      );
-    else if (users.length == 0)
-      return message.channel.send(
-        "<:error:466995152976871434> That user doesn't seem to exist. Try again!"
-      );
-    user = users[0].user;
+  for(let i = 0; i < args.length; i++) {
+    let arg = args[i];
+
+    let user = client.getUserFromMention(arg);
+
+    if(!user) {
+      let usersFound;
+      usersFound = client.searchForMembers(message.guild, arg);
+      if (usersFound.length > 1)
+        return message.channel.send(
+          "<:error:466995152976871434> Found multiple users! Please be more specific or mention the user instead."
+        );
+      else if (usersFound.length == 0)
+        return message.channel.send(
+          "<:error:466995152976871434> That user doesn't seem to exist. Try again!"
+        );
+      user = usersFound[0].user;
+    }
+
+    users.push(user);
+    totalLength += user.username.length;
+  }
+
+  let lengthPerUser = Math.floor(totalLength / users.length);
+
+  let finalName = '';
+
+  let last = -1;
+
+  for(let i = 0; i < users.length; i++) {
+    let user = users[i];
+    let l = Math.min(lengthPerUser, user.username.length);
+
+    let p = user.username.substr(last + 1, last + l);
+
+    console.log(p);
+
+    finalName = finalName + p;
+
+    last = last + l;
   };
 
-  if (!user2) {
-    let users;
-    users = client.searchForMembers(message.guild, args[1]);
-    if (users.length > 1)
-      return message.channel.send(
-        "<:error:466995152976871434> Found multiple users! Please be more specific or mention the user instead."
-      );
-    else if (users.length == 0)
-      return message.channel.send(
-        "<:error:466995152976871434> That user doesn't seem to exist. Try again!"
-      );
-    user2 = users[0].user;
-  };
-
-  var first = user.username.slice(0, user.username.length / 2)
-  var second = user2.username.slice(Math.floor(user2.username.length / 2), user2.username.length)
+  console.log(totalLength);
+  console.log(users.length);
+  console.log(lengthPerUser);
+  console.log(finalName);
 
   try {
-    var attachment = new Discord.MessageAttachment(`https://api.alexflipnote.dev/ship?user=${user.avatarURL({format: "png"})}&user2=${user2.avatarURL({format: "png"})}`)
-    message.channel.send(`Your ship name is **${first+second}!**`, attachment)
+    //var attachment = new Discord.MessageAttachment(`https://api.alexflipnote.dev/ship?user=${user.avatarURL({format: "png"})}&user2=${user2.avatarURL({format: "png"})}`)
+    message.channel.send(`Your ship name is **${finalName}!**`)
     message.channel.stopTyping();
   } catch(err) {
     message.channel.send(`<:error:466995152976871434> API error: ${err}`);
