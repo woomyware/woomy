5d211fcc68d66e5f79ea9f88692be30e7e703e79
TheCakeChicken <cake@cakechicken.me>
2020-03-26 14:05:48 +0000

    make the bot stop typing here as well

--- src/commands/catfact.js
+++ src/commands/catfact.js
@@ -3,14 +3,14 @@ const request = require("request");
 exports.run = async (bot, message, args) => {
   message.channel.startTyping();
   try{
-  request({ uri: "https://catfact.ninja/facts", json: true }, (error, response, body) => {
-    message.channel.send(`**Did you know?**\n ${body.data[0].fact}`);
-    message.channel.startTyping();
-  });
-} catch(err) {
-  message.channel.send(`<:error:466995152976871434> API error: ${err}`);
+    request({ uri: "https://catfact.ninja/facts", json: true }, (error, response, body) => {
+      message.channel.send(`**Did you know?**\n ${body.data[0].fact}`);
+      message.channel.startTyping();
+    });
+  } catch(err) {
+    message.channel.send(`<:error:466995152976871434> API error: ${err}`);
+  };
   message.channel.stopTyping();
-};
 };
 
 exports.conf = {
