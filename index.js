9a71454ec5fb9cc5846e6e381eaa7982185b83f5
Lukáš Horáček <flgx16@gmail.com>
2020-03-30 10:47:55 +0200

    Add comments, basic initialization function to the core

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Woomy version 2
 // Copyright 2020 mudkipscience
 
+// Check node.js version
 if (Number(process.version.slice(1).split('.')[0]) < 13) {
   console.log('NodeJS 12.0.0 or higher is required. Please update NodeJS on your system.')
   process.exit()
 }
 
+// Load environment variables / config
 const fs = require('fs')
 
 if (fs.existsSync('./.env') === false) {
@@ -20,14 +22,24 @@ if (fs.existsSync('./config.js') === false) {
 
 require('dotenv').config()
 
+// Prepare variables
 const Discord = require('discord.js')
 const client = new Discord.Client({ disabledEvents: ['TYPING_START'] })
 
 client.commands = new Discord.Collection()
 client.aliases = new Discord.Collection()
 
+// Initialization function
 const init = async () => {
+  // Register events
+  fs.readdir('./events', (err, files) => {
+    files.forEach(file => {
+      client.on(file.substr(0, file.length - 3), require('./events/' + file))
+    });
+  });
 
+  // Login into Discord
+  client.login(process.env.TOKEN);
 }
 
-init()
+init();
