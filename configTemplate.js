1f267e1cfb1b67634bde546bbbeee01ce1d7aaad
mudkipscience <me@emily.im>
2020-04-03 11:35:54 +1100

    Added configTemplate.js, fixed .gitignore

---
+++ configTemplate.js
@@ -0,0 +1,87 @@
+const config = {
+  // Discord tokens
+  token: '',
+  devtoken: '',
+
+  // API keys that are required for some features/commands
+  keys: {
+    dbl: '', // top.gg key
+    yt: '' // youtube API key
+  },
+
+  // Users added to this embed get access to developer-level commands
+  devs: [''],
+
+  // ID for the support server and the various channels Woomy logs to (leave blank if not used)
+  support: {
+    id: '',
+    logs: '',
+    startupLogs: '',
+    serverLogs: ''
+  },
+
+  // URL of MongoDB database
+  mongoDB: 'mongodb://localhost:27017/woomy',
+
+  // Default prefix
+  prefix: '~',
+
+  // Emojis used by Woomy
+  emojis: {
+    success: '',
+    error: '',
+    denied: '',
+    search: '',
+    crown: '',
+    boost: '',
+    bot: '',
+    status: {
+      online: '',
+      idle: '',
+      dnd: '',
+      offline: ''
+    }
+  },
+
+  // Permission levels
+  permLevels: [
+    {
+      level: 0,
+      name: 'User',
+      check: () => true
+    },
+
+    {
+      level: 1,
+      name: 'Moderator',
+      check: (message) => {
+        try {
+          if (message.member.roles.cache.has(message.settings.modRole)) return true
+        } catch (e) {
+          return false
+        }
+      }
+    },
+
+    {
+      level: 2,
+      name: 'Administrator',
+      check: (message) => {
+        try {
+          if (message.member.roles.cache.has(message.settings.adminRole) || message.member.permissions.has('ADMINISTRATOR')) return true
+        } catch (e) {
+          return false
+        }
+      }
+    },
+
+    {
+      level: 3,
+      name: 'Server Owner',
+
+      check: (message) => message.channel.type === 'text' ? (message.guild.ownerID === message.author.id) : false
+    }
+  ]
+}
+
+module.exports = config
