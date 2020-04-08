1a6cd52d0660f43640a2ec8dd7e349a002a16851
mudkipscience <me@emily.im>
2020-04-09 00:09:55 +1000

    sans

--- commands/help.js
+++ commands/help.js
@@ -11,8 +11,8 @@ exports.help = {
   name: 'help',
   category: 'General',
   description: 'Sends you a list of Woomy\'s commands.',
-  usage: '`help <command>`\n`help all` - View full command list.',
-  params: '`<command>` - The name of the command you want more information on.'
+  usage: '`help` - Lists all commands.\n`help <command>` - Receive more information on a command.',
+  examples: '`help`\n`help roleinfo`'
 }
 
 const Discord = require('discord.js')
@@ -29,75 +29,51 @@ exports.run = (client, message, args, level, data) => {
   const categories = []
 
   if (!args[0]) {
-    embed.setTitle('Woomy Help')
-    embed.setDescription(`• Prefixes: ${'`' + prefixes.join('`, `') + '`'}\n• Use \`help <command>\` to recieve more information about a command!\n• Use \`help all\` to view hidden commands!\n• [Join my support server](https://discord.gg/HCF8mdv) if you have any issues/questions while using me!\n• [click here](https://discordapp.com/oauth2/authorize?client_id=435961704145485835&permissions=8&scope=bot) to add me to your own server!`)
-    const commands = message.guild ? client.commands.filter(
-      cmd => client.levelCache[cmd.conf.permLevel] <= level
-    ) : client.commands.filter(
-      cmd => client.levelCache[cmd.conf.permLevel] <= level && cmd.conf.guildOnly !== true
-    )
-
-    commands.forEach((cmd) => {
-      if (!categories.includes(cmd.help.category)) {
-        categories.push(cmd.help.category)
-      }
-    })
-
-    categories.sort().forEach((cat) => {
-      const filtered = commands.filter((cmd) => cmd.help.category === cat)
-      embed.addField(cat + ` [${filtered.size}]`, filtered.map((cmd) => '`' + cmd.help.name + '`').join(', '), true)
-    })
-
-    if (message.guild && data.guild.customCommands.length > 0) {
-      embed.addField('Custom', data.guild.customCommands.map((cmd) => '`' + cmd.name + '`').join(' '))
-    }
-
-    return message.channel.send(embed)
-  } else if (args[0].toLowerCase() === 'all') {
-    embed.setTitle('Woomy Help')
-    embed.setDescription(`• Prefixes: ${'`' + prefixes.join('`, `') + '`'}\n• Use \`help <command>\` to recieve more information about a command!\n• [Join my support server](https://discord.gg/HCF8mdv) if you have any issues/questions while using me!\n• [click here](https://discordapp.com/oauth2/authorize?client_id=435961704145485835&permissions=8&scope=bot) to add me to your own server!`)
+    embed.setTitle('Woomy Commands')
+    embed.setDescription(`• Prefixes: ${'`' + prefixes.join('`, `') + '`'}\n• Use \`help <command>\` to recieve more information about a command!\n• [Join my support server](https://discord.gg/HCF8mdv)`)
     const commands = client.commands
 
     commands.forEach((cmd) => {
       if (!categories.includes(cmd.help.category)) {
+        if (cmd.help.category === 'Developer' && !client.config.devs.includes('message.author.id')) {
+          return
+        }
         categories.push(cmd.help.category)
       }
     })
 
     categories.sort().forEach((cat) => {
       const filtered = commands.filter((cmd) => cmd.help.category === cat)
-      embed.addField(cat + ` [${filtered.size}]`, filtered.map((cmd) => '`' + cmd.help.name + '`').join(' '))
+      embed.addField(cat, filtered.map((cmd) => '`' + cmd.help.name + '`').join(', '))
     })
 
     if (message.guild && data.guild.customCommands.length > 0) {
       embed.addField('Custom', data.guild.customCommands.map((cmd) => '`' + cmd.name + '`').join(' '))
     }
 
+    embed.addField('Invite', '[Invite](https://discordapp.com/oauth2/authorize?client_id=435961704145485835&permissions=8&scope=bot) | [Discord Server](https://discord.gg/HCF8mdv)')
     return message.channel.send(embed)
-  } else if (args[0]) {
+  } else {
     const command = args.shift().toLowerCase()
     const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
     if (!cmd) {
       return message.channel.send('Command/alias doesn\'t exist')
     }
 
-    let aliases = ''
+    let desc = ''
+
     if (cmd.conf.aliases.length > 0) {
-      aliases = cmd.conf.aliases.join(', ')
+      desc += `*(Aliases: ${cmd.conf.aliases.join(', ')})*\n\n`
     }
 
+    desc += cmd.help.description + `\n\n**You need the \`${cmd.conf.permLevel}\` rank to run this command! This command has a cooldown of \`${cmd.conf.cooldown / 1000}\` seconds per user.**`
+
     embed.setTitle(cmd.help.category.toLowerCase() + ':' + cmd.help.name)
-    embed.setDescription(`*(Aliases: ${aliases})*\n\n${cmd.help.description}`)
-    embed.addField('**Usage:**', cmd.help.usage)
-    if (cmd.help.params.length > 0) {
-      embed.addField('**Parameters:**', cmd.help.params)
+    embed.setDescription(desc)
+    embed.addField('**Usage**', cmd.help.usage)
+    if (cmd.help.examples.length > 0) {
+      embed.addField('**Examples**', cmd.help.examples)
     }
-    if (aliases) {
-      embed.addField('**Aliases:**', aliases)
-    }
-    embed.addField('**Permission level:**', cmd.conf.permLevel, true)
-    embed.addField('**Server only**', cmd.conf.guildOnly, true)
-    embed.addField('**Cooldown**', cmd.conf.cooldown / 1000 + ' seconds', true)
     embed.setFooter('< > = optional, [ ] = required. Don\'t include the brackets in the command itself!')
     message.channel.send(embed)
   }
