5fd8da270277b58eae68c5da25a9aff734aa076e
mudkipscience <me@emily.im>
2020-04-21 17:05:40 +1000

    fixed skip command

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -6,9 +6,9 @@ exports.run = (client, message, args, level) => {
     "<:error:466995152976871434> Nothing is playing."
     );
 
-  let vc = message.guild.members.cache.get(client.user.id).voiceChannel;
+  let vc = message.guild.members.cache.get(client.user.id).voice.channel;
 
-  if(vc != message.member.voiceChannel) return message.channel.send(
+  if(vc != message.member.voice.channel) return message.channel.send(
     '<:error:466995152976871434> You need to be in my voice channel to use this command!'
     );  
 
