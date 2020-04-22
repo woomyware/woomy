e7ed2881394ff33b481fc210791061588100ade1
Lukáš H <flgx16@gmail.com>
2020-04-22 09:49:45 +0200

    Leave if no one is listening

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -7,7 +7,31 @@ module.exports = (client, oldState, newState) => {
         let guild = music.getGuild(newState.guild.id);
 
         if(guild.playing && guild.voiceChannel.id == oldState.channelID) {
-            console.log(newState.id + ' left VC, in which Woomy is playing music');
+            if(guild.voiceChannel.members.size == 1) {
+                guild.autoDisconnect = true;
+                
+                setTimeout(() => {
+                    if(guild.voiceChannel.members.size == 1 && guild.autoDisconnect) {
+                        setTimeout(() => {
+                            if(guild.voiceChannel.members.size == 1 && guild.autoDisconnect) {
+                                setTimeout(() => {
+                                    if(guild.voiceChannel.members.size == 1 && guild.autoDisconnect) {
+                                        // Probably should be async? But no need here I think
+                                        guild.dispatcher.end('silent');
+                                        guild.message.channel.send('No one is listening to me. Leaving voice chat!');
+                                    } else {
+                                        guild.autoDisconnect = false;
+                                    };
+                                }, 30000);
+                            } else {
+                                guild.autoDisconnect = false;
+                            };
+                        }, 20000);
+                    } else {
+                        guild.autoDisconnect = false;
+                    };
+                }, 10000);
+            };
         };
     }
 }  
