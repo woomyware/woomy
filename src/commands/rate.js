3dcdd1f2ad8a1f64ba26c8e9cb01b8f951d64d0f
Emily J <me@emily.im>
2020-08-16 18:20:26 +1000

    rate can no longer ping roles

--- src/commands/rate.js
+++ src/commands/rate.js
@@ -3,23 +3,24 @@ exports.run = async (client, message, args) => {
     return message.channel.send(
         `<:error:466995152976871434> What am I meant to rate? Usage: \`${client.commands.get(`rate`).help.usage}\``
     );
-var rating = [
-    "0/10",
-    "1/10",
-    "2/10",
-    "3/10",
-    "4/10",
-    "5/10",
-    "6/10",
-    "7/10",
-    "8/10",
-    "9/10",
-    "10/10"
-];
+    
+    var rating = [
+        "0/10",
+        "1/10",
+        "2/10",
+        "3/10",
+        "4/10",
+        "5/10",
+        "6/10",
+        "7/10",
+        "8/10",
+        "9/10",
+        "10/10"
+    ];
 
-if (message.content.includes("@everyone") || message.content.includes("@here")) {
-  return message.channel.send('>:(');
-}
+    if (message.content.includes("@everyone") || message.content.includes("@here") || message.content.includes("<@&")) {
+        return message.channel.send('>:(');
+    };
 
     let mess = rating.random();
     message.channel.send(`<:star:618393201501536258> I give ${args.join(" ")} a **${mess}**`);
