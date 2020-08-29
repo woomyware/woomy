6d1d4a04411ab4fd53ee70848e2492a968241e5c
FLGX <flgx@protonmail.com>
2020-08-29 20:58:35 +0200

    Do not exit on uncaughtException
    I think I forgot this here while testing something

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -197,7 +197,6 @@ module.exports = client => {
   process.on("uncaughtException", err => {
     const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
     client.logger.error(`Uncaught Exception: ${errorMsg}`);
-    process.exit(1);
   });
 
   process.on("unhandledRejection", err => {
