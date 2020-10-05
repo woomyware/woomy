05b35fc207b0b0c59d3e5cb5f8e4837109631060
Emily J <me@emily.im>
2020-10-05 18:42:06 +1100

    wtf lol

--- src/commands/coinflip.js
+++ src/commands/coinflip.js
@@ -1,15 +1,4 @@
 exports.run = (client, message, args) => {
-  if(!args[0]) {
-    return message.channel.send(
-      `<:error:466995152976871434> Invalid choice. Usage: \`${client.commands.get(`flip`).help.usage}\``
-    );
-  };
-
-  if(args[0].toLowerCase() != "heads" && args[0].toLowerCase() != "tails") {
-    return message.channel.send(
-      `<:error:466995152976871434> Invalid choice. Usage: \`${client.commands.get(`coinflip`).help.usage}\``
-    );
-  };
   var coin = [
     "Heads!",
     "Tails!"
@@ -31,5 +20,5 @@ exports.help = {
   name: "coinflip",
   category: "Fun",
   description: "Flips a coin!",
-  usage: "coinflip [heads/tails]"
+  usage: "coinflip"
 };
